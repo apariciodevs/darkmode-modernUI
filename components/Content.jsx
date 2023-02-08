cb14b16f037f96fa9939ac8180bdfeec607cd4fe
Facu <Facu@MainMacbookPro.local>
2023-02-08 13:21:03 -0300

    text opacity

--- components/Content.jsx
+++ components/Content.jsx
@@ -12,19 +12,21 @@ const Content = () => {
           <h1 className="mb-10 text-4xl font-semibold md:text-5xl ">
             High speed transactions, everywhere and{" "}
             <span className="leading-snug underline underline-offset-4 lg:underline-offset-8 decoration-neutral-50 dark:decoration-rose-300">
-               anywhere
+              anywhere
             </span>
           </h1>
-          <p className="font-medium leading-6 text-md md:text-xl ">
-            Make and receive payments anywhere on earth with our lightning network. Now supporting
-            transactions on
+          <p className="p-4 font-medium leading-6 dark:bg-gray-900/70 bg-gray-200/80 text-md md:text-xl rounded-3xl">
+            Make and receive payments anywhere on earth with our lightning
+            network. Now supporting transactions on
             <span> Solana</span> and <span>Ethereum.</span>
           </p>
           <div className="flex gap-2 mt-12">
             <button className="px-6 py-2 tracking-tight transition duration-300 dark:bg-neutral-400/40 rounded-2xl text-neutral-50 bg-neutral-900 hover:bg-neutral-800/80 dark:hover:bg-neutral-800/80">
               Get started
             </button>
-            <button className="px-4 py-2 font-semibold tracking-tight hover:underline underline-offset-4 text-neutral-50">Learn more</button>
+            <button className="px-4 py-2 font-semibold tracking-tight hover:underline underline-offset-4 text-neutral-50">
+              Learn more
+            </button>
           </div>
         </div>
       </div>
